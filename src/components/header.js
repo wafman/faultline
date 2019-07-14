2d0bd1d34d79e03961932485ff2c78a0b300f91c
Will Fritts <waf515@gmail.com>
2019-07-13 21:49:13 -0700

    add header component

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from './nav.js';
+
+class Header extends React.Component{
+  render(){
+    return(
+      <>
+        <img src="#" alt="banner image here"></img>  
+        <header><NavBar /></header>
+      </>
+    )
+  }
+}
+
+export default Header;
