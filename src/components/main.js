fb9a1ca7867d97e24f2c0e2f733599722a519b0d
Will Fritts <waf515@gmail.com>
2019-07-13 21:50:17 -0700

    added nav component with basic naviation links-unpopulated so far

---
+++ src/components/main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Main extends React.Component{
+  render(){
+    return(
+      <>
+        <main>
+          <p>the magic will go here</p>
+        </main>
+        
+      </>
+    )
+  }
+}
+
+export default Main;
---
+++ src/components/nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class NavBar extends React.Component{
+  render(){
+    return(
+      <>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
+      </>
+    )
+  }
+}
+
+export default NavBar;
