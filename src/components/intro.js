b6989c82816f653039428ea4371a4c208aad6e3c
Will Fritts <waf515@gmail.com>
2019-07-28 15:01:09 -0700

    created intro component to hold short blurb about band on main paige

---
+++ src/components/intro.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Intro extends React.Component{
+  render(){
+    return(
+      <>
+      <p>the magic will go here</p>
+      </>
+    )
+  }
+}
+
+export default Intro;
