83f307a4f9042cdde4cfa20dda8ddcf59dbf2e88
Will Fritts <waf515@gmail.com>
2019-07-28 15:57:42 -0700

    created media component for routing

---
+++ src/components/media.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Media extends React.Component{
+  render(){
+    return(
+      <>
+        <h1>Congrats, browser router works (media page).</h1>
+      </>
+    )
+  }
+}
