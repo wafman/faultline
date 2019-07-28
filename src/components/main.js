82a553add565446aa7fd6da9b13dd8158258acf5
Will Fritts <waf515@gmail.com>
2019-07-28 15:58:49 -0700

    updated for browser routing

--- src/components/main.js
+++ src/components/main.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import Video from './video.js';
 import Intro from './intro.js';
+import Calendar from './calendar.js';
+import News from './news.js';
 
 class Main extends React.Component{
   render(){
     return(
   
       <main>
-        <Video />
-        <Intro />
+        <div className="videoDiv">
+          <Video />
+          <Intro />
+        </div>
+        <section>
+          <Calendar />
+          <News />
+        </section>
         
       </main>
 
