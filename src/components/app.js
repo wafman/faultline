0c4ee29bb7cdbf052f81aa26a02bf76b8e2a9a1e
Will Fritts <waf515@gmail.com>
2019-07-28 15:56:55 -0700

    instantiated browser routing

--- src/components/app.js
+++ src/components/app.js
@@ -2,13 +2,20 @@ import React from 'react';
 import '../app.scss';
 import Header from './header.js';
 import Main from './main.js';
+import AboutUs from './about-us.js';
+import Media from './media.js'
 import Footer from './footer.js';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header />
-      <Main />
+      <Switch>
+        <Route exact path='/' component={Main}/>
+        <Route path='/about-us' component={AboutUs}/>
+        <Route path='/media' component={Media}/>
+      </Switch>
       <Footer />
     </>
   );
