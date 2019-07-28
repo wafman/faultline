685bc4f1e93b8c52f3e41acaeb996c5ad7119896
Will Fritts <waf515@gmail.com>
2019-07-28 15:59:04 -0700

    updated for browser routing

--- src/components/video.js
+++ src/components/video.js
@@ -4,13 +4,13 @@ class Video extends React.Component{
   render(){
     return(
   
-      <div className="videoDiv">
+      <>
         <h1>Check out a live video of us below</h1>
         <video controls>
           <source src="/../public/Videos/Major-Tom.mp4" type="video/mp4"></source>
           Your browser does not support video
         </video>
-      </div>
+      </>
       
   
     )
