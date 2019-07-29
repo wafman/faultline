bfd0df57512216d38fbd92bf70fda873bd8e7da6
Will Fritts <waf515@gmail.com>
2019-07-28 23:06:08 -0700

    changed to browser router links, inline floated Book us

--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,24 @@ import { Link } from 'react-router-dom';
 
 
 export default class Header extends React.Component{
+
+  
+
   render(){
+    var floatRight = {
+      float: 'right',
+      marginRight: '30px',
+      textDecoration: 'none'
+    };
     return(
       <header>
         <ul>
-          <li><Link to="/"><img src={require('../static/images/faultline.jpg')} alt="Home Button" className="bannerImg"></img></Link></li>
-          <li className="farSide"><Link to="/about-us">About</Link></li> 
-          <li className="farSide"><Link to="/media">Media</Link></li>
+          <li><Link to="/" style={{textDecoration: 'none'}}><img src={require('../static/images/faultline.jpg')} alt="Home Button" className="bannerImg"></img></Link></li>
+          <li className="farSide" style={{textDecoration: 'none'}}><Link to="/about-us">About</Link></li> 
+          <li className="farSide" style={{textDecoration: 'none'}}><Link to="/news">News</Link></li>
+          <li className="farSide" style={{textDecoration: 'none'}}><Link to="/calendar">Calendar</Link></li>
+          {/* <li className="farSide" style={{textDecoration: 'none'}}><Link to="/media">Media</Link></li> */}
+          <li style={floatRight} ><a href="">Book Us</a></li>
         </ul>
         
       </header>
