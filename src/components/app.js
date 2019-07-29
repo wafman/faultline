b01cd5a7971a38ae159bbfc4bb754e9f4eeee095
Will Fritts <waf515@gmail.com>
2019-07-28 23:07:27 -0700

    commented out unused link for now

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../app.scss';
 import Header from './header.js';
-import Main from './main.js';
-import AboutUs from './about-us.js';
+import Main from './main/main.js';
+import AboutUs from './about-us/about-us.js';
 import Media from './media.js'
 import Footer from './footer.js';
+import News from './news.js';
+import Calendar from './calendar.js';
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
         <Route exact path='/' component={Main}/>
         <Route path='/about-us' component={AboutUs}/>
         <Route path='/media' component={Media}/>
+        <Route path='/news' component={News} />
+        <Route path='/calendar' component={Calendar}/>
       </Switch>
       <Footer />
     </>
