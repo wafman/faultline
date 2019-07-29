96a8dcca7616c63abcabb6c78b5611912ebad351
Will Fritts <waf515@gmail.com>
2019-07-28 23:06:34 -0700

    reorganized structure

---
+++ src/components/main/intro.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Intro extends React.Component{
+  render(){
+    return(
+      <>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce elementum augue id ex scelerisque, sit amet sodales ipsum tristique. Mauris varius mi diam, at congue odio sodales ut. Nam non justo consequat, placerat ante quis, ornare urna. Sed quam leo, dictum vel leo vitae, bibendum tincidunt lectus. Nullam vulputate consectetur mauris. Integer aliquam lectus eu lorem elementum, vel porta enim faucibus. Integer sodales, leo at convallis pulvinar, est eros egestas metus, finibus faucibus nulla nunc et ipsum. Etiam tempor ullamcorper diam at commodo. Aliquam ullamcorper, sapien eget faucibus lobortis, libero neque pulvinar nibh, at cursus nisi mi eget enim. Cras mollis lacus sit amet nisl hendrerit, vel aliquet nisl aliquam.
+      </p>
+
+      <p>
+      Praesent tellus mauris, dapibus in aliquam in, suscipit id nibh. Praesent luctus vehicula pharetra. Phasellus magna neque, bibendum sit amet libero vel, aliquet elementum lectus. Donec lectus sapien, pretium ac justo non, cursus volutpat est. Nullam ut ipsum nunc. Sed nec egestas lorem. Mauris ultricies, sem sit amet rutrum pulvinar, metus tortor dapibus arcu, ut sodales quam massa ac odio. Maecenas et sapien nulla.
+      </p>
+
+      <p>
+      Nullam vel tempus velit. Phasellus pretium, quam ac tempor maximus, nisl urna tincidunt tortor, ut sollicitudin ipsum odio eget lectus. Proin iaculis ligula ligula, id convallis enim lobortis et. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent at fermentum metus, eget luctus justo. Sed ornare arcu tortor, vestibulum ornare enim viverra vitae. Phasellus ut lorem vitae elit lobortis feugiat. Nam ac bibendum neque, ut sollicitudin eros. Suspendisse vel massa vestibulum, auctor metus volutpat, lobortis quam. Nullam dignissim sed nibh ac commodo. Morbi congue augue ut porttitor posuere.
+      </p>
+
+      <p>
+      Cras arcu libero, porttitor vel tincidunt id, varius sit amet ligula. Ut molestie accumsan augue, nec eleifend risus ullamcorper eu. Nam sagittis velit at leo tempor, sed tristique tortor interdum. Nam dictum aliquet justo eget volutpat. Vestibulum dictum tortor ut ullamcorper ornare. Mauris id enim vel nunc facilisis pretium id non felis. Fusce vitae nisl sollicitudin, faucibus ligula at, tempor sem. Maecenas sed turpis pulvinar sapien feugiat facilisis.
+      </p>
+
+      <p>
+      Nulla facilisi. Etiam laoreet non nisi a facilisis. Proin bibendum maximus lacus sed malesuada. Morbi at magna aliquam, posuere lectus sed, commodo leo. Suspendisse ac efficitur ligula. Aliquam ac gravida nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna nulla, venenatis mattis lacinia non, tristique ut enim. Sed porta mi mi, sed feugiat leo placerat vel. Ut eget velit diam. Integer venenatis lacus nec magna commodo iaculis. Nullam dictum arcu non eleifend molestie. Pellentesque sapien metus, viverra quis mi a, aliquam vulputate odio.
+      </p>
+      </>
+    )
+  }
+}
+
+export default Intro;
