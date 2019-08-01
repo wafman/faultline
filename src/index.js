99da522ad925ee7f068c9b8a3804ed981482eb0f
Will Fritts <waf515@gmail.com>
2019-08-01 08:34:35 -0700

    imported bootstrap css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
 import App from './components/app.js';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+
 
 ReactDOM.render(
 <BrowserRouter>
