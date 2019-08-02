0fd8aae009c5ed0b2f624148e561ab86f8b4a725
Will Fritts <waf515@gmail.com>
2019-08-01 18:33:46 -0700

    started styling, centered only

--- src/components/news.js
+++ src/components/news.js
@@ -4,13 +4,16 @@ class News extends React.Component{
   render(){
     return(
       <>
-      <h1>News && Announcements</h1>
-      <div className="news">
-        <article>
-          <h3>News Title</h3>
-          <p>News/announcement guts</p>
-        </article>
-      </div>
+        <center>
+            <h1>News && Announcements</h1>
+        </center>
+      
+        <div className="news">
+          <article>
+            <h3>News Title</h3>
+            <p>News/announcement guts</p>
+          </article>
+        </div>
       </>
       
     )
