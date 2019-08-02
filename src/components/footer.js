fb49a6a49aaad3d5175ab6dbd453b47e647dd736
Will Fritts <waf515@gmail.com>
2019-08-01 18:48:20 -0700

    fixed sticky problem and removed links to header

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,10 +9,6 @@ class Footer extends React.Component{
         </div>
         <footer>
           <p>This production is brought to you by William Fritts</p>
-          <a href="#" class="fa fa-facebook"></a>
-          <a href="#" class="fa fa-youtube"></a>
-          {/* <img src="x" alt="youtube link"></img>
-          <img src="x" alt="facebook link"></img> */}
         </footer>
       </>
     )
