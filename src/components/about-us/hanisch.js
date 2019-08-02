87bc5f89d4ae4ef8f7194eb5d741d647caac5978
Will Fritts <waf515@gmail.com>
2019-08-01 18:34:07 -0700

    bootstrap style card

--- src/components/about-us/hanisch.js
+++ src/components/about-us/hanisch.js
@@ -3,14 +3,19 @@ import React from 'react';
 export default class Hanisch extends React.Component{
   render(){
     return(
-      <article>
-        <img src={require('../../static/images/faultline.jpg')} alt="Home Button" className="profileImg"></img>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vehicula nulla sit amet ullamcorper dictum. Nulla et eros eu enim ultrices blandit sed eu metus. Phasellus ac vulputate tellus. Sed varius leo et eleifend luctus. Vivamus aliquet turpis nec dignissim aliquam. Curabitur malesuada ultricies mi ut lacinia. Donec nulla odio, tempor eu porttitor in, accumsan et arcu. Aenean pellentesque aliquet aliquet. Cras tincidunt viverra felis, eget dapibus libero varius sed. Integer posuere lorem id aliquam aliquam.
-
-          Aliquam varius justo sit amet augue dictum semper. Vestibulum ligula mi, tincidunt scelerisque metus eget, cursus accumsan ipsum. Nulla dolor ligula, laoreet at nibh luctus, euismod mattis nunc. Ut maximus elit ex, in convallis arcu interdum vel. Aenean quis aliquam turpis, nec dignissim arcu. Nam consectetur molestie magna et sagittis. Donec ac risus enim. Vestibulum pulvinar, sapien nec euismod auctor, mauris nisl suscipit arcu, varius eleifend eros nunc quis neque. Fusce eu laoreet justo. Sed consequat ornare sapien, at scelerisque velit rhoncus egestas. Etiam ornare nisi vel purus aliquet, in pharetra mauris imperdiet. Fusce consequat, metus et lacinia fermentum, quam nisi ullamcorper augue, nec consequat orci lorem sed leo. Fusce consectetur neque vel egestas laoreet. Sed at hendrerit augue.
-        </p>
-      </article>
+      <div className="aboutUs">
+          <div className="card border-primary mb-3 bandCard" style={{"max-width" : "20rem"}}>
+              <div className="card-header">
+                <h1>Member</h1>
+              </div>
+              <div className="card-body">
+                  <img src={require('../../static/images/faultline.jpg')} alt="Home Button" className="profileImg"></img>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vehicula nulla sit amet ullamcorper dictum. Nulla et eros eu enim ultrices blandit sed eu metus. Phasellus ac vulputate tellus. Sed varius leo et eleifend luctus. Vivamus aliquet turpis nec dignissim aliquam. Curabitur malesuada ultricies mi ut lacinia. Donec nulla odio, tempor eu porttitor in, accumsan et arcu. Aenean pellentesque aliquet aliquet. Cras tincidunt viverra felis, eget dapibus libero varius sed. Integer posuere lorem id aliquam aliquam.
+                  </p>
+              </div>
+          </div>
+      </div>
     )
   }
 }
