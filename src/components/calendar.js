93f59356588edc17a23e3299d32228972873cd4d
Will Fritts <waf515@gmail.com>
2019-08-01 18:30:59 -0700

    styled calendar element

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -4,22 +4,25 @@ class Calendar extends React.Component{
   render(){
     return (
       <>
-      <h1>Calendar</h1>
-      <table className="table table-hover">
-        <tbody>
-          <tr>
-            <th scope="col">Date</th>
-            <th scope="col">Time</th>
-            <th scope="col">Location</th>
-          </tr>
-          <tr>
-            <td>Thursday August 1 2019</td>
-            <td>6:00 pm</td>
-            <td>FOB -- Dupont, WA</td>
-          </tr>
+        <center style={{margin: "30px auto 90px auto"}}>
+        < h1>Calendar</h1>
+        </center>
+        
+        <table className="table table-hover">
+          <tbody>
+            <tr>
+              <th scope="col">Date</th>
+              <th scope="col">Time</th>
+              <th scope="col">Location</th>
+            </tr>
+            <tr>
+              <td>Thursday August 1 2019</td>
+              <td>6:00 pm</td>
+              <td>FOB -- Dupont, WA</td>
+            </tr>
 
-        </tbody>
-      </table>
+          </tbody>
+        </table>
       </>
       
     )
