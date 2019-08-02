1ae4c4f791bfe3ec615ec187029817781b882b19
Will Fritts <waf515@gmail.com>
2019-08-01 18:32:32 -0700

    changed links to icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,10 +4,15 @@ class Footer extends React.Component{
   render(){
     return(
       <>
+        <div class="wrapper">
+          <div class="push"></div>
+        </div>
         <footer>
           <p>This production is brought to you by William Fritts</p>
-          <img src="x" alt="youtube link"></img>
-          <img src="x" alt="facebook link"></img>
+          <a href="#" class="fa fa-facebook"></a>
+          <a href="#" class="fa fa-youtube"></a>
+          {/* <img src="x" alt="youtube link"></img>
+          <img src="x" alt="facebook link"></img> */}
         </footer>
       </>
     )
